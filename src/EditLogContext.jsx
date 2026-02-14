e75b7bdd494cb62f345281f0cdac9ed2fa18b008
supermercado97 <isaiahmercadotx@gmail.com>
2026-02-14 11:57:24 -0600

    removed non-functional code from editlogcontext and removed a shitty pdf box parser prototype

--- src/EditLogContext.jsx
+++ src/EditLogContext.jsx
@@ -3,11 +3,13 @@ import ValidationContext from './ValidationContext';
 
 
 const EditLogContext = ({parsedData}) => {
+    /*
     const editLog = []
     function logEdit({timestamp, field, original_value, new_value, user_initiated}){
         editLog.push({timestamp, field, original_value, new_value, user_initiated})
         console.log("new edit logged")
     }
+    */
     return (
         <div>
             <h1>Edit Log Context</h1>
